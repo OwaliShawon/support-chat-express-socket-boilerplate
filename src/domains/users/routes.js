215af7e1202af525575aa45c4c1043c7eb4cefff
owalishawon <owali.shawon@gmail.com>
2024-12-03 11:12:21 +0600

    [add] request logger

--- src/domains/users/routes.js
+++ src/domains/users/routes.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const { findAll, findById, create, update, remove } = require('./api');
+const { logRequest } = require('../../middlewares/request-log');
+const { validateRequest } = require('../../middlewares/request-validate');
+const getUsersDto = require('./dto');
 
 const router = express.Router();
 
 router
     .route('/')
-    .get(findAll) // GET /users
-    .post(create); // POST /users
+    .get(logRequest({}), validateRequest({ schema: getUsersDto }), findAll) // GET /users
+    .post(logRequest({}), create); // POST /users
 
 router
     .route('/:id')
-    .get(findById) // GET /user:id
-    .patch(update) // PATCH /user:id
-    .delete(remove); // DELETE /user:id
+    .get(logRequest({}), findById) // GET /user:id
+    .patch(logRequest({}), update) // PATCH /user:id
+    .delete(logRequest({}), remove); // DELETE /user:id
 
 module.exports = router;
